7af9eb588dc4480318555d645432b1c68963236c
Sachit Mittal <sachitmittal073@gmail.com>
2025-09-23 14:04:51 +0530

    Initial Commit

---
+++ BuyNow/BuyNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyNowApp.swift
+//  BuyNow
+//
+//  Created by Sachit Mittal on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BuyNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuyNowTests/BuyNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuyNowTests.swift
+//  BuyNowTests
+//
+//  Created by Sachit Mittal on 23/09/25.
+//
+
+import Testing
+@testable import BuyNow
+
+struct BuyNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
