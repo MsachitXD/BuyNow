40c7210e887445ed6578c2aa6ba2ef026f103ac3
Sachit Mittal <sachitmittal073@gmail.com>
2025-09-27 20:25:26 +0530

    new coding

---
+++ BuyNow/BuyNowView.swift
@@ -0,0 +1,18 @@
+//
+//  BuyNowView.swift
+//  BuyNow
+//
+//  Created by Sachit Mittal on 23/09/25.
+//
+
+import SwiftUI
+
+struct BuyNowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuyNowView()
+}
