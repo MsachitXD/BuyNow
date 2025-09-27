0beb1173d42c91c442fc19795b754a4dbce93bec
Sachit Mittal <sachitmittal073@gmail.com>
2025-09-27 20:25:42 +0530

    new coding

--- BuyNow/BuyNowView.swift
+++ BuyNow/BuyNowView.swift
@@ -8,11 +8,89 @@
 import SwiftUI
 
 struct BuyNowView: View {
+    
+    let price : Double
+    let discountPercentage : Int?
+    var discountedPrice : Double? {
+        if let discountPercentage {
+            // $20 -> (25 / 100) * $20 = $5
+            return (Double(100 - discountPercentage) / 100) * price
+        } else {
+            return nil
+        }
+    }
+    let discountColor: Color
+    let bgColor: Color
+    let yOffset : Double
+    let discountAngle: Double
+    
+    init(price: Double, discountPercentage: Int?, discountColor: Color = .blue, bgColor: Color = .red, yOffset: Double = -20, discountAngle: Double = -10) {
+        self.price = price
+        self.discountPercentage = discountPercentage
+        self.discountColor = discountColor
+        self.bgColor = bgColor
+        self.yOffset = yOffset
+        self.discountAngle = discountAngle
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            if let discountPercentage {
+                Text("\(discountPercentage)% OFF!")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .background(discountColor)
+                    .clipShape(.rect(cornerRadius: 15))
+                    .rotationEffect(Angle(degrees: discountAngle))
+                    .offset(y: yOffset)
+                    .shadow(radius: 5)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            
+            HStack{
+                Text("BUY NOW")
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Text("$\(price, specifier: "%.2f")")
+                    .foregroundColor(.white)
+                    .strikethrough(discountedPrice != nil)
+                
+                if let discountedPrice {
+                    Text("$\(discountedPrice, specifier: "%.2f")")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+            }
+        }
+        .padding()
+        .background(
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(bgColor)
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(.white)
+
+            }
+        )
+        .padding()
     }
 }
 
 #Preview {
-    BuyNowView()
+    ZStack{
+        Color.black.opacity(0.7).ignoresSafeArea()
+        
+        VStack{
+            BuyNowView(
+                price: 20, discountPercentage: 75
+            )
+            
+            BuyNowView(
+                price: 20, discountPercentage: nil
+            )
+        }
+    }
 }
